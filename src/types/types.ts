c4c984d9e925142a972912427a22e53e6bfbf66c
Joon Ho Cho <joonho1101@gmail.com>
2023-03-15 17:51:39 +0900

    types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,17 @@
+export type Primitive = undefined | null | boolean | number | bigint | string;
+
+export type KeyType = Primitive;
+
+export type TypeOfReturn =
+  | 'undefined'
+  | 'boolean'
+  | 'number'
+  | 'bigint'
+  | 'string'
+  | 'symbol'
+  | 'object'
+  | 'function';
+
 export type KeyOf<T> = keyof T & string;
 
 export type KeyOfType<T, V> = keyof {
@@ -10,12 +24,21 @@ export type KeyOfExcludeType<T, V> = keyof {
 
 export type AnyFunction = (...args: any[]) => any;
 
+export type VoidFunction = () => void;
+
+export type VoidFn = VoidFunction;
+
+export type PromiseReturnType<T extends PromiseLike<T>> = T extends
+  | Promise<infer Return>
+  | PromiseLike<infer Return>
+  ? Return
+  : T;
+
+export type ItemType<T extends any[]> = T extends Array<infer Item>
+  ? Item
+  : never;
+
 export interface IPoint {
   x: number;
   y: number;
 }
-
-export type VoidFunction = () => void;
-export type VoidFn = VoidFunction;
-
-export type KeyType = string | number | boolean | null | undefined;
