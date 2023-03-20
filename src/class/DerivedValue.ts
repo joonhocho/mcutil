07ee4b5eb9b07df7ef8f7ba838c4a486e3df4ca6
Joon Ho Cho <joonho1101@gmail.com>
2023-03-20 12:48:25 +0900

    cleanup DerivedValue using CleanUpMap

--- src/class/DerivedValue.ts
+++ src/class/DerivedValue.ts
@@ -1,3 +1,4 @@
+import { CleanUpMap } from './CleanUpMap.js';
 import { IValueOptions, Value } from './Value.js';
 
 export type ToValue<T> = { [P in keyof T]: Value<T[P]> };
@@ -5,9 +6,8 @@ export type ToValue<T> = { [P in keyof T]: Value<T[P]> };
 const getValue = <T>(x: Value<T>): T => x.value;
 
 export class DerivedValue<T, Deps extends Array<any>> extends Value<T> {
-  private _value?: Value<T>;
-  private _off?: () => void;
-  private _offs: Array<() => void>;
+  protected _val?: Value<T>;
+  protected _cleaup = new CleanUpMap();
 
   constructor(
     public deps: ToValue<Deps>,
@@ -19,49 +19,39 @@ export class DerivedValue<T, Deps extends Array<any>> extends Value<T> {
     super(value instanceof Value ? value.value : value, options);
 
     if (value instanceof Value) {
-      this._value = value;
-      this._off = value.on((v) => this.set(v));
+      this._val = value;
+      this._cleaup.set(
+        'value',
+        value.on((v) => this.set(v))
+      );
     }
 
-    this._offs = deps.map((x) => x.on(this.syncValue));
+    this._cleaup.set(
+      'deps',
+      deps.map((x) => x.on(this.syncValue))
+    );
   }
 
   destroy(): void {
-    if (this._value) {
-      this._value = undefined;
-    }
-
-    if (this._off) {
-      this._off();
-      this._off = undefined;
-    }
-
-    const { _offs } = this;
-    for (let i = 0, il = _offs.length; i < il; i += 1) {
-      _offs[i]();
-    }
-    this._offs.length = 0;
-
+    if (this._val) this._val = undefined;
+    this._cleaup.destroy();
     super.destroy();
   }
 
   syncValue = (): void => {
     const value = this.selector(...(this.deps.map(getValue) as Deps));
     if (value instanceof Value) {
-      if (value !== this._value) {
-        if (this._off) {
-          this._off();
-          this._off = undefined;
-        }
-        this._value = value;
-        this._off = value.on((v) => this.set(v));
+      if (value !== this._val) {
+        this._val = value;
+        this._cleaup.set(
+          'value',
+          value.on((v) => this.set(v))
+        );
       }
+
       this.value = value.value;
     } else {
-      if (this._off) {
-        this._off();
-        this._off = undefined;
-      }
+      this._cleaup.clear('value');
       this.value = value;
     }
   };
